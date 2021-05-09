7585b2ef544d2415288ad880fc6bd06b13540a1c
Mito6211 <willmoore674@gmail.com>
2021-05-09 16:49:39 -0400

    Create/add logo, favicon, and manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Real Renting</title>
+        <meta
+          name="description"
+          content="Real Estate property management service for investors with multiple properties"
+        />
+        <meta name="theme-color" content="#1b1b1e" />
+        <meta name="apple-mobile-web-app-status-bar" content="#1b1b1e" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo_96x96.png" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
-import Head from "next/head";
 import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Real Renting</title>
-        <meta
-          name="description"
-          content="Real Estate property management service for investors with multiple properties"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <Heading>Real Renting</Heading>
       </main>
